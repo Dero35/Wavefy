bb76aebd9d2b48fc64f38170790b46ae1a11845f
Dero35 <antek.ciesielski.2006@gmail.com>
2022-03-14 18:56:10 +0100

    added hero section to index.html

--- menu-script.js
+++ menu-script.js
@@ -5,10 +5,3 @@ menuCta.addEventListener("click", function () {
   menuCta.classList.toggle("is-active");
   menu.classList.toggle("active");
 });
-
-/*const menu = document.querySelector(".navbar-menu");
-
-function menuCtaActive() {
-  let menuCta = document.querySelector(".navbar-menu-cta");
-  menuCta.classList.toggle("is-active");
-}*/
